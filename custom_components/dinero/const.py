d0f73bf41bba79e9c54fc977fcc4a5c5c9f101b1
Jopppardk <rene@sablen.dk>
2026-08-24 13:47:07 +0200

    Add configurable inventory adjustment

--- custom_components/dinero/const.py
+++ custom_components/dinero/const.py
@@ -7,6 +7,12 @@ CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"
 CONF_API_KEY = "api_key"
 CONF_ORGANIZATION_ID = "organization_id"
+CONF_INVENTORY_ACCOUNT = "inventory_account"
+CONF_INVENTORY_ADJUSTMENT_ACCOUNT = "inventory_adjustment_account"
+CONF_INVENTORY_SOURCE_ENTITY = "inventory_source_entity"
+
+DEFAULT_INVENTORY_ACCOUNT = 52000
+DEFAULT_INVENTORY_ADJUSTMENT_ACCOUNT = 2450
 
 API_BASE_URL = "https://api.dinero.dk/v1"
 TOKEN_URL = "https://authz.dinero.dk/dineroapi/oauth/token"
