a75513a13d379792c0b214c9f52c9d382cbe4180
Jopppardk <rene@sablen.dk>
2026-08-24 06:20:05 +0200

    Add Dinero integration implementation

---
+++ custom_components/dinero/const.py
@@ -0,0 +1,14 @@
+"""Constants for the Dinero integration."""
+
+from datetime import timedelta
+
+DOMAIN = "dinero"
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"
+CONF_API_KEY = "api_key"
+CONF_ORGANIZATION_ID = "organization_id"
+
+API_BASE_URL = "https://api.dinero.dk/v1"
+TOKEN_URL = "https://authz.dinero.dk/dineroapi/oauth/token"
+DEFAULT_SCAN_INTERVAL = timedelta(minutes=15)
+
